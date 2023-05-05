e9265356c76e09976b78bc445b6c257d45bd6dc4
Truong Thanh Thien <thien.truongthanh@elca.vn>
2023-05-05 11:28:17 +0700

    Kiem tra a va xuat ket qua

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello Thien");
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello Thien");
+
+int a = 1;
+
+if (a > 0)
+{
+    Console.WriteLine("a > 0");
+}
+else if (a < 0)
+{
+    Console.WriteLine("a < 0");
+}
+else
+    Console.WriteLine("a = 0");
